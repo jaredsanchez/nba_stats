b154fb2f26b4f54cfe78519ea4d9e365fbc174e2
jaredsanchez <jared.d.sanchez@gmail.com>
2015-08-26 20:30:52 -0700

    Made the search bar functional, adding selctability to selectable elements

--- vis/js/index.js
+++ vis/js/index.js
@@ -25,8 +25,168 @@ var data = {
 };
 
 
+
+// SEARCH BAR VARS
+var teams
+var teamNames
+// var teamIDs
+var players
+var playerNames
+// var playerIDs
+var selectables
+
+function getChildren(n, skipMe){
+    var r = [];
+    for ( ; n; n = n.nextSibling ) 
+       if ( n.nodeType == 1 && n != skipMe)
+          r.push( n );        
+    return r;
+};
+function getSiblings(n) {
+    return getChildren(n.parentNode.firstChild, n);
+}
+
+
+// Function to create arrays for search bar lookup
+function setNames() {
+    teams = mappings["teams"];
+    teamNames = [];
+    teamIDs = [];
+    for (team in teams) {
+        teamNames.push({'value':teams[team]['team_city']+' '+teams[team]['team_name'], 'data': team})
+        // teamIDs.push(team)
+    };
+
+    players = mappings["players"];
+    playerNames = []
+    playerIDs = []
+    for (player in players) {
+        playerNames.push({'value':players[player]['player_name'], 'data':player})
+        // playerIDs.push(player)
+    };
+}
+
+// function to determine if search bar should use team or player array
+function determineLookup() {
+    if (document.getElementById("teams").classList.contains('selected')) {
+        return teamNames
+    }
+    else if (document.getElementById("players").classList.contains('selected')) {
+        return playerNames
+    }
+    else {
+        return []
+    }
+}
+
+// function called when elem is clicked to make items selectable
+function addSelectable(elem) {
+    if (elem.classList.contains("selected")){
+        elem.classList.remove("selected");
+    }
+    else {
+        elem.classList.add("selected");
+    }
+}
+
+// function to handle adding selected class to team/player
+function tpAddSelectable(elem) {
+    if (elem.id === 'teams') {
+        if (elem.classList.contains("selected")){
+            return
+        }
+        else {
+            elem.classList.add("selected");
+            elem.nextElementSibling.classList.remove("selected");
+            $('#autocomplete').autocomplete().setOptions({'lookup': determineLookup()});
+        }
+    } else {
+        if (elem.classList.contains("selected")){
+            return
+        }
+        else {
+            elem.classList.add("selected");
+            elem.previousElementSibling.classList.remove("selected");
+            $('#autocomplete').autocomplete().setOptions({'lookup': determineLookup()});
+        }
+    }
+}
+
+// function to handle adding selected class to seasons
+function sAddSelectable(elem) {
+    if (elem.id === 'all') {
+        if (elem.classList.contains("selected")){
+            return
+        } else {
+            elem.classList.add("selected");
+            var nextElem = elem.nextElementSibling;
+            while (nextElem !== null) {
+                nextElem.classList.remove("selected");
+                nextElem = nextElem.nextElementSibling;
+            }
+        }
+    } else {
+        if (elem.classList.contains("selected")){
+            var siblings = getSiblings(elem);
+            for (var i=0; i<siblings.length; i++) {
+                if (siblings[i].classList.contains("selected")) {
+                    elem.classList.remove("selected");
+                    return;
+                }
+            }
+        } else {
+            elem.classList.add("selected");
+            if (document.getElementById("all").classList.contains("selected")) {
+                document.getElementById("all").classList.remove("selected")
+            }
+        }
+    }
+}
+
+
+
+
 window.onload = function() {
+    // CHART STUFF
     var ctx = document.getElementById("myChart").getContext("2d");
     var myNewChart = new Chart(ctx).Line(data);
 
+    // MAKE ITEMS SELECTABLE
+    // makeSelectable();
+
+    // SEARCH BAR STUFF
+    setNames();
+
+    $('#autocomplete').autocomplete({
+        lookup: determineLookup(),
+        onSelect: function (suggestion) {
+            alert('You selected: ' + suggestion.value + ', ' + suggestion.data);
+        }
+    });
+
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
